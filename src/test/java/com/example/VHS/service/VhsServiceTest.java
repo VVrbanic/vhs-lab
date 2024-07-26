31b3c7e44ac14e027e41d895d370d06a56ef3478
DarleneOasu <kakosusvaimenaiskoristena@gmail.com>
2024-07-26 23:45:12 +0200

    New tests added

--- src/test/java/com/example/VHS/service/VhsServiceTest.java
+++ src/test/java/com/example/VHS/service/VhsServiceTest.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 class VhsServiceTest {
@@ -53,6 +56,30 @@ class VhsServiceTest {
 
         // then
         assertEquals(createdVhs, newVhs);
+
+    }
+    @Test
+    void DoesItGetAllUsers(){
+        // given
+        Vhs vhs1 = new Vhs();
+        vhs1.setName("New movie 1");
+        vhs1.setTotalNumber(3);
+        vhs1.setNumberInStock(3);
+        Vhs createdVhs1 = vhsRepository.save(vhs1);
+
+        Vhs vhs2 = new Vhs();
+        vhs2.setName("New movie 2");
+        vhs2.setTotalNumber(5);
+        vhs2.setNumberInStock(5);
+        Vhs createdVhs2 = vhsRepository.save(vhs2);
+
+        // when
+        List<Vhs> allVhs = underTest.getAllVhs();
+
+        // then
+        assertTrue(allVhs.contains(createdVhs1));
+        assertTrue(allVhs.contains(createdVhs2));
+        assertEquals(2, allVhs.size());
 
     }
 
